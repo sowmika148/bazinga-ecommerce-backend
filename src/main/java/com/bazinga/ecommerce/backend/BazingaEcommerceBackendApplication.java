91306e6948504741560e5189909d6030acac4c6b
ext_tsowmika <ext_tsowmika@falabella.cl>
2020-08-16 12:14:16 +0530

    BAZ-1 | Add dependencies-springweb, actuator, h2, flyway

---
+++ src/main/java/com/bazinga/ecommerce/backend/BazingaEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bazinga.ecommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazingaEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bazinga/ecommerce/backend/BazingaEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazinga.ecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazingaEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
